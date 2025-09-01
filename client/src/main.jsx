452edcc7e5cf7d7be9096d39aa7be4f8120d9709
HARSHAVAR2608 <128389644+HARSHAVAR2608@users.noreply.github.com>
2025-09-01 22:56:14 +0530

    Added backend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -9,3 +9,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css'; // Make sure you have this CSS file
+
+// This is the standard and correct way to initialize a React 18 app.
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
